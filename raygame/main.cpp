0baf816d72244203e885efd53cc1978a5e9f6f61
Terry Nguyen <terreh@terrehbyte.com>
2018-12-06 23:34:14 -0800

    Add demo code for dot products

--- raygame/main.cpp
+++ raygame/main.cpp
@@ -9,8 +9,28 @@
 *
 ********************************************************************************************/
 
+#include <cstdio>
+
 #include "tools.h"
 #include "raylib.h"
+#include "raymath.h"
+
+void DrawArrow(int startPosX, int startPosY, int endPosX, int endPosY, Color color)
+{
+    DrawLine(startPosX, startPosY, endPosX, endPosY, color);
+    DrawCircleLines(endPosX, endPosY, 5.0f, color);
+}
+
+void DrawArrowEx(Vector2 startPos, Vector2 endPos, float thick, Color color)
+{
+    DrawLineEx(startPos, endPos, thick, color);
+    DrawCircleV(endPos, thick * 1.25f, color);
+}
+
+Vector2 Vector2FromVector3(Vector3 vec)
+{
+    return {vec.x, vec.y};
+}
 
 int main()
 {
@@ -24,6 +44,18 @@ int main()
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
+    Vector2 origin = {float(screenWidth / 2), float(screenHeight / 2)};
+    Vector2 vecA = {0, 1};
+    Vector2 vecB = {1, 0};
+    float arrowLength = float((screenHeight < screenHeight ? screenHeight : screenWidth) / 4);
+    float arrowThick = 10.0f;
+    float rotationSpeed = 1.0f;
+
+    const size_t TEXT_BUFFER_SIZE = 30;
+    char vecAtext[TEXT_BUFFER_SIZE];
+    char vecBtext[TEXT_BUFFER_SIZE];
+    char dotText[TEXT_BUFFER_SIZE];
+
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
@@ -32,13 +64,34 @@ int main()
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
 
+        float aAxis = IsKeyDown('Q') ? -1.0f : 0.0f + IsKeyDown('E') ? 1.0f : 0.0f;
+        float bAxis = IsKeyDown('A') ? -1.0f : 0.0f + IsKeyDown('D') ? 1.0f : 0.0f;
+
+        float rotDelta = rotationSpeed * GetFrameTime();
+
+        vecA = Vector2FromVector3(Vector3Transform({vecA.x, vecA.y, 0.0f}, MatrixRotateZ(-aAxis * rotDelta)));
+        vecB = Vector2FromVector3(Vector3Transform({vecB.x, vecB.y, 0.0f}, MatrixRotateZ(-bAxis * rotDelta)));
+
+        float dot = Vector2DotProduct(vecA, vecB);
+
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawArrowEx(origin, Vector2Add(origin, Vector2Scale(vecA, arrowLength)), arrowThick, RED);
+        DrawArrowEx(origin, Vector2Add(origin, Vector2Scale(vecB, arrowLength)), arrowThick, BLUE);
+
+        sprintf_s(vecAtext, TEXT_BUFFER_SIZE, "%f , %f", vecA.x, vecA.y);
+        sprintf_s(vecBtext, TEXT_BUFFER_SIZE, "%f , %f", vecB.x, vecB.y);
+        sprintf_s(dotText, TEXT_BUFFER_SIZE, "%f", dot);
+
+        DrawText(vecAtext, 10, 10, 20, BLUE);
+        DrawText(vecBtext, 10, 40, 20, RED);
+        DrawText(dotText, 10, 70, 20, GREEN);
+
+        
 
         EndDrawing();
         //----------------------------------------------------------------------------------
